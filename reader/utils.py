b3ace6c4be1a0e8546b3d5a27df5bd39680993b3
Jannik Stebani <jannik.stebani@physik.uni-wuerzburg.de>
2021-01-21 19:28:59 +0100

    Add various helper functions

--- reader/utils.py
+++ reader/utils.py
@@ -8,6 +8,81 @@ from typing import List, Dict, Tuple, Union, Iterable, Any
 Several utility functions and definitions
 """
 
+def is_binary(array: np.ndarray) -> bool:
+    """
+    Check if given array is binary. Performs a 'soft check',
+    i.e. if all elements are clos to either 1 or 0.
+    """
+    if np.all(np.logical_or(np.isclose(array, 1), np.isclose(array, 0))):
+        return True
+    else:
+        return False
+
+
+def convert_to_intlabel(array: np.ndarray) -> np.ndarray:
+    """
+    Convert an onehot-encoded label array of the form (C x [... spatial ...])
+    to consecutive integer label array.
+    Different classes/channels along the first axis are labeled with consecutive
+    integers.
+
+    Parameters
+    ----------
+
+    array : np.ndarray
+        The input array. Expected to be one-hot-encoded and
+        of the form (C x [...spatial...])
+    
+    Returns
+    -------
+
+    intlabel_array : np.ndarray
+        The converted array with consecutive integer labels.
+        has the form ([...spatial...]) due to reduction
+        over channel axis.
+    """
+    assert is_binary(array), 'Expecting a binary-like array'
+    # reduce over channel axis
+    contracted_shape = array.shape[1:]
+    intlabel_array = np.zeros(contracted_shape, dtype=np.int)
+    # zero stays zero: background
+    for idx in range(1, array.shape[0]):
+        intlabel_array[array[idx, ...]] = idx
+    return intlabel_array
+    
+
+def convert_to_onehot(array: np.ndarray) -> np.ndarray:
+    """
+    Convert a multi-integer label array to its one-hot encoded
+    representation.
+    The number of channels/classes are deduced automatically from the
+    unique elements in the input array.
+    The returned array has the shape (C x *original_shape*).
+    The binary masks along the channel dimensions are ordered
+    following the label integers.
+
+    Parameters
+    ----------
+
+    array : numpy.ndarray
+        The multi-integer label array.
+
+    
+    Returns
+    -------
+
+    onehot_array : numpy.ndarray
+        The one-hot-encoded representation
+    """
+    labelvalues = np.unique(array)
+    expanded_shape = (len(labelvalues),) + array.shape
+    onehot_array = np.zeros(expanded_shape, dtype=np.int)
+    for idx, labelval in enumerate(labelvalues):
+        onehot_array[idx, array == labelval] = 1
+    return onehot_array
+
+
+
 def homogenize(segmentation_data_list: List) -> None:
     """
     Homogenize segment label_value and naming schemes that might differ
